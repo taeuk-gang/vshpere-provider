b72df85fceaf2f40571569775f7a58c372dc7bb5
강태욱 <twkang@mantech.co.kr>
2024-02-11 02:01:12 +0900

    test

--- variables.tf
+++ variables.tf
@@ -21,12 +21,12 @@ variable "vm_mem" {
 
 variable "vm_domain" {
   type    = string
-  default = ".acloud.lab"
+  default = ".domain"
 }
 
 variable "public_key" {
  type = string
- default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDAqgwd1wyvsj9bHjmhC6Kxa5vhH7H1YQDIln5uciZbPhwTJh3g8T/eSB6jzFxBAsmGfPdE8YzhVk729npE7r+O1vpfi1pDoqCTYxV8Yp/IebyjtKoDNkJO1o7bExIxFhH3daGjhzPkGML8HADmT86yX24NFIjOlQTd2/p4SWwVYByg1CWJWyB4FsjanZUCA2Vhz7oQnzGL7X1m+vDqBncKNjwds1r5IC0ZCWWxbYxpbg/VYONVddDuB2rO9fFXc9vNdb1alC0uIk1UGapfivjvYHXx3deeZukuaMkPPreqm/qVswzLB5+uT+0fAFqisk8oGC9UkWuvioGKUHtC7kB+fZGjyfiPgoBtB53dCz3ZKkhIxAOhqcEJO9KOUcnVUGyOo3I9R7jCKbRulun2IWpeYwtk5aD07Hz8MwHf/YRdhrHO/XFXNLXgPlO2wfAyLukiRNHOStvB5TYNlG4GNwU00jnybcShO7TrZ61ajsjpCLi5hCmikz2zIeMksj+24ZE= ansil@ACLOUDLAB-MSTR"
+ default = "ssh-rsa test local@local"
 }
 
 variable "ssh_username" {
@@ -34,50 +34,50 @@ variable "ssh_username" {
 }
 variable "vm_password" {
   type = string
-  default = "Ubuntu@123"
+  default = "password"
 }
 
 variable "vm_net" {
   type    = string
-  default = "DSwitch-VM Network"
+  default = "vm Network"
 }
 
 variable "vm_template" {
   type    = string
-  default = "ubuntu-jammy-22.04-cloudimg"
+  default = "template"
 }
 
 variable "vsphere_dc" {
   type    = string
-  default = "ACloudLabDC"
+  default = "dc"
 }
 
 variable "vsphere_datastore" {
   type    = string
-  default = "vsanDatastore"
+  default = "store"
 }
 
 variable "vsphere_user" {
   type    = string
-  default = "administrator@acloud.lab"
+  default = "root@server"
 }
 
 variable "vsphere_password" {
   type    = string
-  default = "Vmware@123"
+  default = "password"
 }
 
 variable "vsphere_server" {
   type    = string
-  default = "vc.acloud.lab"
+  default = "server"
 }
 
 variable "vsphere_cluster" {
   type = string
-  default = "ACloudLabCluster"
+  default = "cluster"
 }
 
 variable "vsphere_resource_pool" {
   type = string
-  default = "ACloudLabResourcePool"
+  default = "resourcePool"
 }
