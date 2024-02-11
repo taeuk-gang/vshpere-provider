d9b954a5ac08b600cca292176c700358ec9870ea
강태욱 <twkang@mantech.co.kr>
2024-02-11 20:24:58 +0900

    test

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ resource "vsphere_virtual_machine" "vm" {
     properties = {
       "hostname" = var.name
       "public-keys" = var.public_key
-      "user-data" = var.user_data
+      "user-data" = data.template_cloudinit_config.config.rendered
       "password" = var.vm_password
     }
   }
