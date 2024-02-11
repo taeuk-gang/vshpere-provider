01f56aabfa39d9182d6ef0189480ad1f2341b8fb
강태욱 <twkang@mantech.co.kr>
2024-02-11 18:26:54 +0900

    test

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
 output "private_ip" {
   description = "The default IP address of the virtual machine deployed"
-  value = vsphere_virtual_machine.vm.default_ip_address
+  value = vsphere_virtual_machine.vm.guest_ip_addresses[0]
 }
 
 output "public_ip" {
   description = "The default IP address of the virtual machine deployed"
-  value = vsphere_virtual_machine.vm.default_ip_address
+  value = vsphere_virtual_machine.vm.guest_ip_addresses[0]
 }
 
 output "hostname" {
